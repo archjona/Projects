621ff416e194789f5dd63862f55cf0a260026597
archjona <jona@cadenbach.eu>
2026-02-25 17:36:11 +0100

    I2C compensation

--- I2C/build/esp-idf/esp_app_format/CMakeFiles/__idf_esp_app_format.dir/flags.make
+++ I2C/build/esp-idf/esp_app_format/CMakeFiles/__idf_esp_app_format.dir/flags.make
@@ -8,5 +8,5 @@ C_INCLUDES = -I/home/jona/esp-idf/Projects/I2C/build/config -I/home/jona/esp-idf
 
 C_FLAGS = @"/home/jona/esp-idf/Projects/I2C/build/toolchain/cflags" -fdiagnostics-color=always -ffunction-sections -fdata-sections -Wall -Werror -Wno-error=unused-function -Wno-error=unused-variable -Wno-error=unused-but-set-variable -Wno-error=deprecated-declarations -Wextra -Wno-error=extra -Wno-unused-parameter -Wno-sign-compare -Wno-enum-conversion -gdwarf-4 -ggdb -Og -fno-shrink-wrap -fmacro-prefix-map=/home/jona/esp-idf/Projects/I2C=. -fmacro-prefix-map=/home/jona/esp-idf=/IDF -fstrict-volatile-bitfields -fno-jump-tables -fno-tree-switch-conversion -std=gnu23 -Wno-old-style-declaration -fzero-init-padding-bits=all -fno-malloc-dce
 
-# Custom defines: esp-idf/esp_app_format/CMakeFiles/__idf_esp_app_format.dir/esp_app_desc.c.obj_DEFINES = PROJECT_VER="f2932ae1-dirty"; PROJECT_NAME="I2C"
+# Custom defines: esp-idf/esp_app_format/CMakeFiles/__idf_esp_app_format.dir/esp_app_desc.c.obj_DEFINES = PROJECT_VER="8145130b-dirty"; PROJECT_NAME="I2C"
 
--- I2C/main/I2C.c
+++ I2C/main/I2C.c
@@ -1,9 +1,23 @@
-// Programm zum lernen von I2C anhand des BME280 Sensors und zum lernen von
-// ERROR handling beim ESP32
+// Programm zum lernen von I2C anhand des BME280 Sensors
 
 #include "driver/i2c_master.h" // esp32 = master, BME280 = Slave --> nur master treiber nötig
 #include "freertos/FreeRTOS.h"
 #include <stdio.h>
+
+typedef struct {
+  uint16_t dig_T1;
+  int16_t dig_T2;
+  int16_t dig_T3;
+  uint8_t dig_H1;
+  int16_t dig_H2;
+  uint8_t dig_H3;
+  int16_t dig_H4;
+  int16_t dig_H5;
+  int8_t dig_H6;
+} bme280_calib_data_t;
+
+static bme280_calib_data_t cal;
+static int32_t t_fine;
 
 static const i2c_port_num_t i2c_port =
     0; // port 0 (erster I2C controller vom esp32)
@@ -27,6 +41,9 @@ static const uint8_t reg_addr =
           // bereiches, jedes Register 1 byte, 20 bits temp, 20 pressure, 16
           // humidity
 static uint8_t data[8];
+
+float compensate_T(int32_t adc_T);
+float compensate_H(int32_t adc_H);
 
 void app_main(void) {
 
@@ -73,19 +90,89 @@ void app_main(void) {
   // temp, nochmal 001 macht oversampling auf 1x für pressure, 11 setzt den
   // modus des sensors auf "normal", 001 001 11 = 0x27 -> 0x27 muss in register
   // 0xF4 geschrieben werden
+
   i2c_master_transmit(dev_handle, config_meas, 2, -1);
+
+  uint8_t reg;
+  uint8_t t_cal[6], h_cal_part1, h_cal_part2[7];
+
+  reg = 0x88; // Temp Kalibrierung
+  i2c_master_transmit_receive(dev_handle, &reg, 1, t_cal, 6, -1);
+  cal.dig_T1 = (t_cal[1] << 8) | t_cal[0];
+  cal.dig_T2 = (int16_t)((t_cal[3] << 8) | t_cal[2]);
+  cal.dig_T3 = (int16_t)((t_cal[5] << 8) | t_cal[4]);
+
+  reg = 0xA1; // Hum H1
+  i2c_master_transmit_receive(dev_handle, &reg, 1, &h_cal_part1, 1, -1);
+  cal.dig_H1 = h_cal_part1;
+
+  reg = 0xE1; // Hum H2..H6
+  i2c_master_transmit_receive(dev_handle, &reg, 1, h_cal_part2, 7, -1);
+  cal.dig_H2 = (int16_t)((h_cal_part2[1] << 8) | h_cal_part2[0]);
+  cal.dig_H3 = h_cal_part2[2];
+  cal.dig_H4 = (int16_t)((h_cal_part2[3] << 4) | (h_cal_part2[4] & 0x0F));
+  // 0x0F (0000 1111) wird als bit mask verwendet, weil H4 aus 12 Bits besteht
+  // und die unteren 4 bits nur die unteren vom 8 bit 0xE5 register sein sollen
+  cal.dig_H5 = (int16_t)((h_cal_part2[5] << 4) | (h_cal_part2[4] >> 4));
+  cal.dig_H6 = (int8_t)h_cal_part2[6];
 
   printf("BME280 aufgeweckt...\n");
 
   while (1) {
+    // 8 Bytes lesen: Press(0-2), Temp(3-5), Hum(6-7)
+    i2c_master_transmit_receive(dev_handle, &reg_addr, 1, data, 8, -1);
 
-    i2c_master_transmit(dev_handle, &reg_addr, 1, -1);
-    // der write buffer muss auf die anfangsadresse zeigen, also dem register ab
-    // dem gelesen werden soll
-    i2c_master_receive(dev_handle, data, 8, -1);
-    uint16_t bme_H = ((uint16_t)data[6] << 8) | data[7];
-    printf("Feuchtigkeit (raw): %d\n", bme_H);
+    // Rohwerte aus dem Buffer ziehen
+    // Temperatur sind 20 Bit (data[3], data[4] und die oberen 4 Bit von
+    // data[5])
+    int32_t adc_T = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
+    // Feuchtigkeit sind 16 Bit (data[6] und data[7])
+    int32_t adc_H = (data[6] << 8) | data[7];
 
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // Kompensation anwenden
+    float real_T = compensate_T(adc_T); // Berechnet auch t_fine
+    float real_H = compensate_H(adc_H);
+
+    printf("Temp: %.2f °C | Feuchte: %.2f %%\n", real_T, real_H);
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
+}
+
+// mathematische Kompensation (Formeln aus Datenblatt)
+float compensate_T(int32_t adc_T) {
+  int32_t var1, var2;
+  var1 =
+      ((((adc_T >> 3) - ((int32_t)cal.dig_T1 << 1))) * ((int32_t)cal.dig_T2)) >>
+      11;
+  var2 = (((((adc_T >> 4) - ((int32_t)cal.dig_T1)) *
+            ((adc_T >> 4) - ((int32_t)cal.dig_T1))) >>
+           12) *
+          ((int32_t)cal.dig_T3)) >>
+         14;
+  t_fine = var1 + var2;
+  return (float)((t_fine * 5 + 128) >> 8) / 100.0;
+}
+
+float compensate_H(int32_t adc_H) {
+  int32_t v_x1_u32r;
+  v_x1_u32r = (t_fine - ((int32_t)76800));
+  v_x1_u32r =
+      (((((adc_H << 14) - (((int32_t)cal.dig_H4) << 20) -
+          (((int32_t)cal.dig_H5) * v_x1_u32r)) +
+         ((int32_t)16384)) >>
+        15) *
+       (((((((v_x1_u32r * ((int32_t)cal.dig_H6)) >> 10) *
+            (((v_x1_u32r * ((int32_t)cal.dig_H3)) >> 11) + ((int32_t)32768))) >>
+           10) +
+          ((int32_t)2097152)) *
+             ((int32_t)cal.dig_H2) +
+         8192) >>
+        14));
+  v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) *
+                             ((int32_t)cal.dig_H1)) >>
+                            4));
+  v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
+  v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
+  return (float)((uint32_t)(v_x1_u32r >> 12)) / 1024.0;
 }
